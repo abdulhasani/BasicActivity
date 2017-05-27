51d5bc6adfde3c004a6c07c182e707c781ef913e
BHasani <abdul.kadir.hasani@basajans.com>
2017-05-28 04:13:03 +0700

    output data

--- app/src/main/java/com/bebrayan/com/androidcomponent/MainActivity2.java
+++ app/src/main/java/com/bebrayan/com/androidcomponent/MainActivity2.java
@@ -2,6 +2,7 @@ package com.bebrayan.com.androidcomponent;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends Activity {
 
@@ -9,5 +10,15 @@ public class MainActivity2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Bundle data = getIntent().getExtras();
+
+        TextView name = (TextView) findViewById(R.id.nameValue);
+        TextView age = (TextView) findViewById(R.id.umurValue);
+        TextView gender = (TextView) findViewById(R.id.jenisKelaminValue);
+
+        name.setText(data.getCharSequence("nama"));
+        age.setText(data.getCharSequence("umur"));
+        gender.setText(data.getCharSequence("jeniskelamin"));
+
     }
 }
